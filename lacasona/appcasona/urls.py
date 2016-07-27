505b9b451502d76f240611d04941190d427f08b4
Rafalp190 <leo13361@uvg.edu.gt>
2016-07-26 18:09:28 -0600

    Git inicial con carpetas y Proyecto de Django

---
+++ lacasona/appcasona/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+from appcasona import views
+
+urlpatterns = [
+               url(r'^$', views.log_in, name='login'),
+               url(r'^init/', views.log_out, name='logout'),
+               url(r'^index/', views.index, name='index')
+               ]
