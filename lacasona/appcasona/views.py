c18f66ca9473b7423a1fb9a88dd9dd186d8411a8
Rafalp190 <leo13361@uvg.edu.gt>
2016-07-27 20:44:58 -0600

    Se unifico el trabajo de moi con el de rafa

--- lacasona/appcasona/views.py
+++ lacasona/appcasona/views.py
@@ -13,35 +13,35 @@ from django.http import HttpResponse
 # Create your views here.
 def log_in(request):
     #Maneja los errores 
-#     def errorHandle(error):
-#         return render(request, 'appcasona/login.html', {'error' : error,}) 
-#     #Si el usuario no ha iniciado sesion.
-#     if not request.user.is_authenticated():
-#         #Si ya se ha dado ingresado una cuenta
-#         if request.method == 'POST':
-#             username = request.POST['user']
-#             password = request.POST['password']
-#             user = authenticate(username=username, password=password)
-#             if user is not None:
-#                 if user.is_active:
-#                     #Redirige a la pagina correcta.
-#                     login(request, user)
-#                     return render(request, 'appcasona/index.html', {'user': user,})
-#                 else:
-#                     #Retorna un mensaje de error de 'disable account'
-#                     error = u'Su cuenta se encuentra desactivada.'
-#                     return errorHandle(error)
-#             else:
-#                 #Retorna un mensaje de error de 'invalid login'
-#                 error = u'Su usuario o contraseña no son correctos. Por favor intentelo de nuevo.'
-#                 return errorHandle(error)
-#         else:
-#             return render(request, 'appcasona/login.html')
-#     else:
-#         return render(request, 'appcasona/index.html')
+    def errorHandle(error):
+        return render(request, 'appcasona/login.html', {'error' : error,}) 
+    #Si el usuario no ha iniciado sesion.
+    if not request.user.is_authenticated():
+        #Si ya se ha dado ingresado una cuenta
+        if request.method == 'POST':
+            username = request.POST['user']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    #Redirige a la pagina correcta.
+                    login(request, user)
+                    return render(request, 'appcasona/index.html', {'user': user,})
+                else:
+                    #Retorna un mensaje de error de 'disable account'
+                    error = u'Su cuenta se encuentra desactivada.'
+                    return errorHandle(error)
+            else:
+                #Retorna un mensaje de error de 'invalid login'
+                error = u'Su usuario o contrasena no son correctos. Por favor intentelo de nuevo.'
+                return errorHandle(error)
+        else:
+            return render(request, 'appcasona/login_template.html')
+    else:
+        return render(request, 'appcasona/index.html')
 
 #REMOVE WHEN LOGIN TEMPLATE IS COMPLETE
-    return render(request, 'appcasona/login.html')
+#    return render(request, 'appcasona/login_template.html')
     
 def log_out(request):
     logout(request)
