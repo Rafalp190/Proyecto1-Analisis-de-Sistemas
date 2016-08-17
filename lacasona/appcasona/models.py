20d4b48cec6c500b41e368787368a123d3243d65
aa-oliva <oli12149@uvg.edu.gt>
2016-08-17 16:23:56 -0600

    asd

--- lacasona/appcasona/models.py
+++ lacasona/appcasona/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Inventario (models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    content = models.CharField(max_length=255, blank=True)
+    date = models.BigIntegerField(blank=True, null=True)
+    recipients = models.CharField(max_length=255, blank=True)
+    status = models.IntegerField(blank=True, null=True)
+    subject = models.CharField(max_length=255, blank=True)
+    sender = models.ForeignKey('Emailaccount', blank=True, null=True)
+    class Meta:
+        managed = True
+        db_table = 'email'
