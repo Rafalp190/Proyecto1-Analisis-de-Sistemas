6f3ac770074f35b52964f3cd5c212986fc61254c
aa-oliva <oli12149@uvg.edu.gt>
2016-08-17 16:36:37 -0600

    models v2

--- lacasona/appcasona/models.py
+++ lacasona/appcasona/models.py
@@ -2,7 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# Create your models here.'
+
+#idproducto, cantidad de producto
 class Inventario (models.Model):
     id = models.BigIntegerField(primary_key=True)
     content = models.CharField(max_length=255, blank=True)
@@ -11,6 +13,34 @@ class Inventario (models.Model):
     status = models.IntegerField(blank=True, null=True)
     subject = models.CharField(max_length=255, blank=True)
     sender = models.ForeignKey('Emailaccount', blank=True, null=True)
+    class Meta:
+        managed = True
+        db_table = 'email'
+
+#idproducto, id platillo, cantidad utilizada, idingrediente
+class Ingredientes (models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    content = models.CharField(max_length=255, blank=True)
+    date = models.BigIntegerField(blank=True, null=True)
+    recipients = models.CharField(max_length=255, blank=True)
+    status = models.IntegerField(blank=True, null=True)
+    subject = models.CharField(max_length=255, blank=True)
+    sender = models.ForeignKey('Emailaccount', blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'email'
+
+#cliente, nit, id platillo, id mesero
+class Orden (models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    content = models.CharField(max_length=255, blank=True)
+    date = models.BigIntegerField(blank=True, null=True)
+    recipients = models.CharField(max_length=255, blank=True)
+    status = models.IntegerField(blank=True, null=True)
+    subject = models.CharField(max_length=255, blank=True)
+    sender = models.ForeignKey('Emailaccount', blank=True, null=True)
+
     class Meta:
         managed = True
         db_table = 'email'
