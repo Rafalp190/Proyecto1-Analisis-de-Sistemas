089dbedb8bccbc3bd5ef80020d9d7cba2b8da103
Rafalp190 <leo13361@uvg.edu.gt>
2016-08-17 20:16:59 -0600

    Cambios a los models

--- lacasona/appcasona/models.py
+++ lacasona/appcasona/models.py
@@ -8,7 +8,7 @@ from django import forms
  
 #idproducto, cantidad de producto, nombre del producto 
 class Inventario (models.Model):
-    idProducto = models.BigIntegerField(primary_key=True,verbose_name='ID Test')
+    idProducto = models.BigIntegerField(primary_key=True,verbose_name='ID')
     cantidadDeProducto = models.BigIntegerField(db_column='cantidadDeProducto',blank=True,verbose_name='\tCantidad\t')
     nombreDelProducto = models.CharField(max_length=255, blank=True,verbose_name='\tNombre\t',db_column='nombreDelProducto')
      
