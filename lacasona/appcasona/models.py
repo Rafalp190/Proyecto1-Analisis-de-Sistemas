ef501427045a68551e7bfb1eb2bd0b53bb2a6526
aa-oliva <oli12149@uvg.edu.gt>
2016-08-27 20:14:56 -0600

    models y tables
    Se cambiaron las tablas y models por el nuevo diseño

--- lacasona/appcasona/models.py
+++ lacasona/appcasona/models.py
@@ -5,38 +5,40 @@ from django import forms
 
 
 # Create your models here.'
- 
-#idproducto, cantidad de producto, nombre del producto 
-class Inventario (models.Model):
-    idProducto = models.BigIntegerField(primary_key=True,verbose_name='ID')
-    cantidadDeProducto = models.BigIntegerField(db_column='cantidadDeProducto',blank=True,verbose_name='Cantidad')
-    nombreDelProducto = models.CharField(max_length=255, blank=True,verbose_name='Nombre',db_column='nombreDelProducto')
-     
+
+# idproducto, cantidad de producto, nombre del producto
+class Inventario(models.Model):
+    idProducto = models.BigIntegerField(primary_key=True, verbose_name='ID')
+    nombreDelProducto = models.CharField(max_length=255, blank=True, verbose_name='Nombre', db_column='nombreDelProducto')
+    cantidadDeProducto = models.BigIntegerField(db_column='cantidadDeProducto', blank=True, verbose_name='Cantidad')
+    seccion = models.CharField(max_length=255, blank=True, verbose_name='Seccion', db_column='Seccion')
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+
     class Meta:
         managed = True
         db_table = 'inventario'
- 
-#idproducto, id platillo, cantidad utilizada, idingrediente
-class Ingredientes (models.Model):
-    idIngrediente = models.BigIntegerField(primary_key=True,verbose_name='ID')
-    nombreDelProducto = models.CharField(max_length=255, blank=True,verbose_name='Nombre',db_column='nombreDelProducto')
-    idPlatillo = models.BigIntegerField(db_column='idPlatillo',verbose_name='Id Platillo')
-    idProducto = models.BigIntegerField(db_column='idProducto',blank=True,verbose_name='Id Producto')
-    cantidadUtilizada = models.BigIntegerField(db_column='cantidadUtilizada',blank=True,verbose_name='Cantidad')
-     
- 
+
+
+# idproducto, id platillo, cantidad utilizada, idingrediente
+class Proveedor(models.Model):
+    idProveedor = models.BigIntegerField(primary_key=True, verbose_name='ID')
+    nombreDelProveedor = models.CharField(max_length=255, blank=True, verbose_name='Nombre',db_column='nombreDelProveedor')
+    numeroDelProveedor = models.BigIntegerField(db_column='numeroDelProveedor', verbose_name='Numero')
+    direccionDelProveedor = models.CharField(max_length=255, db_column='direccionDelProveedor', verbose_name='Direccion')
+
     class Meta:
         managed = True
-        db_table = 'ingrediente'
- 
-#cliente, nit, id platillo, id mesero
-class Orders (models.Model):
-    cliente = models.CharField(max_length=255, db_column = 'cliente', verbose_name = 'Cliente')
-    nit = models.BigIntegerField (db_column = 'nit',blank = True, verbose_name= 'Nit')
-    factura = models.BigIntegerField (primary_key=True, blank = True, verbose_name= 'Factura')
-    idPlatillo = models.BigIntegerField(db_column='idPlatillo',blank=True,verbose_name='Id Platillo')
-    idMesero= models.BigIntegerField (db_column='idMesero',blank=True,verbose_name='Id Mesero')
+        db_table = 'proveedor'
+
+
+# cliente, nit, id platillo, id mesero
+class Platillo(models.Model):
+    idPlatillo = models.BigIntegerField(primary_key=True, verbose_name='ID')
+    nombreDelPlatillo = models.CharField(max_length=255, db_column='NombreDelPlatillo', verbose_name='Nombre')
+    precioPlatillo = models.BigIntegerField(db_column='PrecioDelPlatillo', blank=True, verbose_name='precio')
+    imagenPlatillo = models.CharField(max_length=255, db_column='ImagenPlatillo', verbose_name='Imagen')
+    ingredientes = models.ForeignKey(Inventario, on_delete=models.CASCADE)
+
     class Meta:
         managed = True
-        db_table = 'ordenes'
-        
+        db_table = 'platillo'
--- lacasona/appcasona/tables.py
+++ lacasona/appcasona/tables.py
@@ -5,30 +5,37 @@ from .models import Inventario
 from django import forms
 from django.utils.html import escape
 
-class IngredientesTable (dt2.Table):
-    idIngrediente = dt2.Column(accessor='idIngrediente')
+
+class InventarioTable(dt2.Table):
+    idProducto = dt2.Column(accessor='idProducto')
     nombreDelProducto = dt2.Column(accessor='nombreDelProducto')
-    cantidadUtilizada = dt2.Column(accessor='cantidadUtilizada')
-    idPlatillo = dt2.Column(accessor='idPlatillo')
-    idProducto = dt2.Column(accessor='idProducto')
-    def __init__(self, *args, **kwargs):
-        super(IngredientesTable, self).__init__(*args, **kwargs)
-        self.counter = itertools.count()
-    
-class InventarioTable (dt2.Table):
-    idProducto = dt2.Column(accessor='idProducto')
     cantidadDeProducto = dt2.Column(accessor='cantidadDeProducto')
-    nombreDelProducto = dt2.Column(accessor='nombreDelProducto')
+    seccion = dt2.Column(accessor='seccion')
+    proveedor = dt2.Column(accessor='proveedor')
+
     def __init__(self, *args, **kwargs):
         super(InventarioTable, self).__init__(*args, **kwargs)
         self.counter = itertools.count()
 
-class OrdenesTable (dt2.Table):
-    factura = dt2.Column(accessor= 'factura')
-    cliente = dt2.Column(accessor='cliente')
-    nit = dt2.Column(accessor= 'nit')
-    idPlatillo = dt2.Column(accessor='idPlatillo')
-    idMesero= dt2.Column(accessor='idMesero')
+
+class ProveedorTable(dt2.Table):
+    idProveedor = dt2.Column(accessor='idProveedor')
+    nombreDelProveedor = dt2.Column(accessor='nombreDelProveedor')
+    numeroDelProveedor = dt2.Column(accessor='numeroDelProveedor')
+    direccionDelProveedor = dt2.Column(accessor='direccionDelProveedor')
+
     def __init__(self, *args, **kwargs):
-        super(OrdenesTable, self).__init__(*args, **kwargs)
+        super(ProveedorTable, self).__init__(*args, **kwargs)
+        self.counter = itertools.count()
+
+
+class PlatilloTable(dt2.Table):
+    idPlatillo = dt2.Column(accessor='idPlatillo')
+    nombreDelPlatillo = dt2.Column(accessor='nombreDelPlatillo')
+    precioPlatillo = dt2.Column(accessor='precioPlatillo')
+    imagenPlatillo = dt2.Column(accessor='imagenPlatillo')
+    ingredientes = dt2.Column(accessor='ingredientes')
+
+    def __init__(self, *args, **kwargs):
+        super(PlatilloTable, self).__init__(*args, **kwargs)
         self.counter = itertools.count()
