87b32e392f2662a31a9c0a28856adce8c31e076f
Rafalp190 <leo13361@uvg.edu.gt>
2016-08-31 22:46:32 -0600

    Se hicieron cambios a las URLs y se agrego un blank value a la tabla de platillos

--- lacasona/appcasona/models.py
+++ lacasona/appcasona/models.py
@@ -43,7 +43,7 @@ class Platillo(models.Model):
     nombreDelPlatillo = models.CharField(max_length=255, db_column='NombreDelPlatillo', verbose_name='Nombre')
     precioPlatillo = models.BigIntegerField(db_column='PrecioDelPlatillo', blank=True, verbose_name='precio')
     imagenPlatillo = models.CharField(max_length=255, db_column='ImagenPlatillo', verbose_name='Imagen')
-    descripcionPlatillo = models.CharField(max_length=255, db_column='DescripcionDelPlatillo', verbose_name='Descripcion')
+    descripcionPlatillo = models.CharField(max_length=255, blank=True, db_column='DescripcionDelPlatillo', verbose_name='Descripcion')
     ingredientes = models.ManyToManyField(Inventario)
 
     class Meta:
--- lacasona/appcasona/urls.py
+++ lacasona/appcasona/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from appcasona import views
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
                url(r'^$', views.log_in, name='login'),
                url(r'^init/', views.log_out, name='logout'),
                url(r'^index/', views.index, name='index'),
-               url(r'^inventory/', views.Inventory.as_view(), name='inventory'),
-               url(r'^proveedor/', views.Proveedor.as_view(), name='proveedor'),
-               url(r'^platillo/', views.Platillo.as_view(), name='platillo')
+               url(r'^inventory/', RedirectView.as_view(url="/admin/appcasona/inventario/"), name='inventory'),
+               url(r'^proveedor/', RedirectView.as_view(url="/admin/appcasona/proveedor/"), name='proveedor'),
+               url(r'^platillo/', RedirectView.as_view(url="/admin/appcasona/platillo/"), name='platillo')
                ]
