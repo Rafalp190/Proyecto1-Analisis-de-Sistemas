8c11db9fcf45624dcf1b7e2afc679d8e094ae866
Rafalp190 <leo13361@uvg.edu.gt>
2016-11-10 16:44:25 -0600

    Se agrego la view de decripcion del menu sans botones de

--- lacasona/appcasona/urls.py
+++ lacasona/appcasona/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
                url(r'^menu/', views.menu, name='menu'),
                url(r'^inventory/', RedirectView.as_view(url="/admin/appcasona/inventario/"), name='inventory'),
                #url(r'^proveedor/', RedirectView.as_view(url="/admin/appcasona/proveedor/"), name='proveedor'),
+               url(r'^(?P<platillo_id>[0-9]+)/descripcion/$', views.platillo_detail, name='descripcion'),
                url(r'^platillo/', RedirectView.as_view(url="/admin/appcasona/platillo/"), name='platillo'),
                url(r'^orden/', RedirectView.as_view(url="/admin/appcasona/orden/"), name='orden')
                ]
